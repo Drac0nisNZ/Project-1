c0cc18b8ba20193f20276e2954faa5e1e427ac60
NathanielGeiger <Nathaniel-Geiger@live.nmit.ac.nz>
2024-09-11 10:59:03 +1200

    started js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const modal = document.getElementById('modal') //declaring variable and linking to the modal class from the html
+const modalClose = document.getElementById('modal-close') //declaring variable and linking to the modal-close class from the html
+const bookmarkForm = document.getElementById('bookmark-form') //declaring variable and linking to the bookmark-form class from the html
+const websiteNameEl = document.getElementById('website-name') //declaring variable and linking to the website-name class from the html
+const websiteUrlEl = document.getElementById('website-url') //declaring variable and linking to the website-url class from the html
+const bookmarksContainer = document.getElementById('bookmarks-container') //declaring variable and linking to the bookmarks-container class from the html
