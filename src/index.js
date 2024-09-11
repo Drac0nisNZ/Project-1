5a99f2e84deec50ec000e2fc6cfa25d5d25be31a
NathanielGeiger <Nathaniel-Geiger@live.nmit.ac.nz>
2024-09-12 10:41:14 +1200

    regex js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const modal = document.getElementById('modal') //declaring variable and linking to the modal class from the html
-const showModal = document.getElementById('showModal')
+const modalShow = document.getElementById('show-modal')
 const modalClose = document.getElementById('modal-close') //declaring variable and linking to the modal-close class from the html
 const bookmarkForm = document.getElementById('bookmark-form') //declaring variable and linking to the bookmark-form class from the html
 const websiteNameEl = document.getElementById('website-name') //declaring variable and linking to the website-name class from the html
@@ -13,6 +13,22 @@ function showModal() {
     websiteNameEl.focus() // shows the tag as a website link
 }
 
+modalShow.addEventListener("click", showModal) // shows or hides the modal card based on it's current state
+modalClose.addEventListener("click", () => {
+    modal.classList.remove("show-modal")
+})
+
+window.addEventListener("click", (e) => { // closes the modal if the window is clicked on
+    e.target === modal ? modal.classList.remove("show-modal") : false
+})
+
 modalClose.addEventListener("click", () => {  // clicking on the X will close the bookmark or bookmarking page
     modal.classList.remove("show-modal")
 })
+
+function validate(nameValue, urlValue) {
+    const expression = //verifies that the url has the correct characters in it
+        /(https)?:\/\/(www\.)?[-a-zA-Z0-9@:%>_+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/g
+
+    const regex
+}
