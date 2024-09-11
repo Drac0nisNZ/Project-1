1eb0bf4d9339c8471c40f63d0ebbc3b7617fb351
NathanielGeiger <Nathaniel-Geiger@live.nmit.ac.nz>
2024-09-12 10:48:26 +1200

    regex finished

--- src/index.js
+++ src/index.js
@@ -30,5 +30,16 @@ function validate(nameValue, urlValue) {
     const expression = //verifies that the url has the correct characters in it
         /(https)?:\/\/(www\.)?[-a-zA-Z0-9@:%>_+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/g
 
-    const regex
+    const regex = new RegExp(expression)
+
+    if(!nameValue || !urlValue) { // checks for correct input in the entry boxes
+        alert("Please submit values for both fields.")
+        return false
+    }
+    if(!urlValue.match(regex)){
+        alert("Please provide a valid web address.")
+        return false
+    }
+    // Valid
+    return true
 }
